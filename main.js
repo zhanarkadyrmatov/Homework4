adf85b7fff7a62bf9c18edd2e27fc5af10c9d42a
Kadyrmatov <janarbek.bekmyrzauulu.alt@gmail.com>
2022-06-06 08:18:28 +0600

    add file

---
+++ main.js
@@ -0,0 +1,8 @@
+// Напишите функцию, которая в цикле выводит числа от 0 до 50
+
+function myNumber() {
+  for (let i = 0; i <= 50; i++) {
+    console.log(i);
+  }
+}
+myNumber();
---
+++ main2.js
@@ -0,0 +1,8 @@
+// Напишите функцию, которая в цикле выводит числа от 50 до 0
+
+function myNumber() {
+  for (let i = 50; i > 0; i--) {
+    console.log(i);
+  }
+}
+myNumber();
